d43a2655fcc4eeaec0a43df5902830248e3d8cc8
Maciek Borzecki <maciek.borzecki@gmail.com>
2024-12-24 15:20:48 +0100

    starting out
    Signed-off-by: Maciek Borzecki <maciek.borzecki@gmail.com>

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use std::error::Error;
+use std::fmt;
+use std::io;
+use std::process::Command;
+
+#[derive(Debug, Clone)]
+struct LxcCommandError {
+    stderr: Vec<u8>,
+    exit_code: i32,
+}
+
+#[derive(Debug)]
+enum LxcError {
+    Start(io::Error),
+    Execution(LxcCommandError),
+}
+
+impl Error for LxcError {}
+
+impl From<io::Error> for LxcError {
+    fn from(error: io::Error) -> Self {
+        LxcError::Start(error)
+    }
+}
+
+impl fmt::Display for LxcError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            LxcError::Execution(eerr) => {
+                write!(
+                    f,
+                    "lxc command exited with status {}, stderr: {}",
+                    eerr.exit_code,
+                    String::from_utf8_lossy(&eerr.stderr)
+                )
+            }
+            LxcError::Start(ioerr) => ioerr.fmt(f),
+        }
+    }
+}
+
+fn run_lxc(args: &[&str]) -> Result<Vec<u8>, LxcError> {
+    let res = Command::new("lxc").args(args).output()?;
+
+    if !res.status.success() {
+        return Err(LxcError::Execution(LxcCommandError {
+            stderr: res.stderr,
+            exit_code: res.status.code().unwrap_or(255),
+        }));
+    }
+    return Ok(res.stdout);
+}
+
+fn main() {
+    let out = run_lxc(&["foo"]).expect("lxc failed");
+    println!("lxc output:\n{}", String::from_utf8_lossy(&out));
+}
